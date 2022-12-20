f69980bcf1d93902cbb4e25af5ef904b7a2c0840
jaehoonKim <temp7045@gmail.com>
2022-12-20 10:47:15 +0900

    feat(_document): add _document.tsx for perfomance

---
+++ src/pages/_document.tsx
@@ -0,0 +1,12 @@
+import { Html, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="ko">
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
