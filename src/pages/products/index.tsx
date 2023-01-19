41b46daa6cd0155891b36a5b91dfb1324cd8b5e2
kimja01 <kimja7045@naver.com>
2023-01-19 20:24:40 +0900

    fix(product): update ProductListPage conflict

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,8 +1,8 @@
 import { useMemo } from 'react'
 import { useInfiniteQuery } from 'react-query'
-import { getProducts } from "@/api/product"
+import { getProducts } from '@/api/product'
 import Image from 'next/image'
-import { IProduct } from "@/types/product.type"
+import { IProduct } from '@/types/product.type'
 
 const TAKE = 9
 export default function ProductListPage() {
@@ -27,35 +27,11 @@ export default function ProductListPage() {
     <div className="px-36 my-36">
       {products && (
         <div className="grid grid-cols-3 gap-5">
-<<<<<<< HEAD
-          {products.map((pd) => (
-            <div key={pd.id}>
-              <Image
-                className="rounded"
-                src={pd.image_url ?? ''}
-                alt={pd.name}
-                width={300}
-                height={200}
-                placeholder="blur"
-                blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mM8p6NTDwAEnQGnwx5bWgAAAABJRU5ErkJggg=="
-              />
-              <div className="flex">
-                <span>{pd.name}</span>
-                <span className="ml-auto">
-                  {pd?.price?.toLocaleString('ko-KR')}원
-                </span>
-              </div>
-              <span className="text-zinc-400">
-                {pd.category_id === '1' && '의류'}
-              </span>
-            </div>
-          ))}
-=======
           {products.map(
             (pd, i) =>
               i < 9 && (
                 <div key={pd.id}>
-                   <Image
+                  <Image
                     className="rounded"
                     src={pd.image_url ?? ''}
                     alt={pd.name}
@@ -76,7 +52,6 @@ export default function ProductListPage() {
                 </div>
               )
           )}
->>>>>>> 9d7e64a865ceb00c77a10d880a6be6e0545bdce3
         </div>
       )}
       <button
