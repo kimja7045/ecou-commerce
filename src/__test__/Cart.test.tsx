da45a0800b55b43825ce54f1ae0137ab75cedc02
kimja01 <kimja7045@naver.com>
2023-01-24 21:38:39 +0900

    feat(test, Cart): add addItem func test

--- src/__test__/Cart.test.tsx
+++ src/__test__/Cart.test.tsx
@@ -9,5 +9,8 @@ describe('Cart', () => {
 
   it('adds an item', () => {
     cart = cart.addItem({ productId: 1, quantity: 1 });
+    expect(cart.items).toHaveLength(1);
+    cart = cart.addItem({ productId: 2, quantity: 1 });
+    expect(cart.items).toHaveLength(2);
   });
 });
