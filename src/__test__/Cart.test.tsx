a080a30f375530894fceda31b24065255c595568
kimja01 <kimja7045@naver.com>
2023-02-12 14:09:08 +0900

    feat(ProductList): seperate ProductListPage & ProductList

--- src/__test__/Cart.test.tsx
+++ src/__test__/Cart.test.tsx
@@ -1,13 +1,13 @@
 import Cart from '@/models/Cart';
 
-describe('Cart', () => {
+describe('장바구니', () => {
   let cart: Cart;
 
   beforeEach(() => {
     cart = new Cart();
   });
 
-  it('adds an item', () => {
+  it('상품을 장바구니에 추가합니다.', () => {
     cart = cart.addItem({ productId: 1, quantity: 1 });
     expect(cart.items).toHaveLength(1);
     cart = cart.addItem({ productId: 2, quantity: 1 });
