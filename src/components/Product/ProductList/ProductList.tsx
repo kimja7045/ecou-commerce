9973cdd8ab5528f96d142d7ce1836a162c24bbff
kimja01 <kimja7045@naver.com>
2023-02-12 14:40:22 +0900

    fix(Product): update Editor Button path

--- src/components/Product/ProductList/ProductList.tsx
+++ src/components/Product/ProductList/ProductList.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-import { IProduct } from '../../../types/product';
+import { IProduct } from '@/types/product';
 
 type ProductListProps = {
   products: IProduct[] | null;
