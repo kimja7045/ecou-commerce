8f864adafe6a5d030141f52dfdfc2162d27e6988
kimja01 <kimja7045@naver.com>
2023-02-16 02:31:25 +0900

    feat(Product): add ProductItem

--- src/components/Product/ProductList/ProductList.tsx
+++ src/components/Product/ProductList/ProductList.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Image from 'next/image';
 import { IProduct } from '@/types/product';
+import ProductItem from './ProductItem';
 
 type ProductListProps = {
   products: IProduct[];
@@ -13,26 +13,7 @@ export const ProductList = ({ products, onClickMoreBtn }: ProductListProps) => {
       {products && (
         <div className="grid grid-cols-3 gap-5">
           {products.map((pd) => (
-            <div key={pd.id}>
-              <Image
-                className="rounded"
-                src={pd.image_url ?? ''}
-                alt={pd.name}
-                width={300}
-                height={200}
-                placeholder="blur"
-                blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mM8p6NTDwAEnQGnwx5bWgAAAABJRU5ErkJggg=="
-              />
-              <div className="flex">
-                <span>{pd.name}</span>
-                <span className="ml-auto">
-                  {pd.price.toLocaleString('ko-KR')}원
-                </span>
-              </div>
-              <span className="text-zinc-400">
-                {pd.category_id === 1 ? '의류' : '기타'}
-              </span>
-            </div>
+            <ProductItem key={pd.id} {...pd} />
           ))}
         </div>
       )}
