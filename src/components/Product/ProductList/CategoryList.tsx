5eac4249d8795e4004beb418d240b4e2933e75b9
kimja01 <kimja7045@naver.com>
2023-02-22 00:58:47 +0900

    refactor(ProductList > CategoryList): remove unused import

--- src/components/Product/ProductList/CategoryList.tsx
+++ src/components/Product/ProductList/CategoryList.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction } from 'react';
+import React from 'react';
 import { SegmentedControl } from '@mantine/core';
 import { categories } from '@prisma/client';
 
