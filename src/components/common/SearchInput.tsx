892df00fe78666bf49d7e6ed87f0f973f0069bb5
kimja01 <kimja7045@naver.com>
2023-03-01 23:40:56 +0900

    feat(lib): remove @tabler/icons install

--- src/components/common/SearchInput.tsx
+++ src/components/common/SearchInput.tsx
@@ -1,14 +1,11 @@
 import { Input } from '@mantine/core';
-// import { IconSearch } from '@tabler/icons';
 import React from 'react';
 
 const SearchInput = () => {
   return (
     <div className="mb-4">
-      <Input
-        //   icon={<IconSearch />}
-        placeholder="Eco Pants"
-      />
+      <Input placeholder="Eco Pants" />
+      {/* <Input icon={<IconSearch />} placeholder="Eco Pants" /> */}
     </div>
   );
 };
