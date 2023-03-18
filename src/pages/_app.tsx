2eb68aa3b86f8912a01226910af447d488d2a617
김재훈 <jhkim05@miridih.com>
2023-03-19 02:49:48 +0900

    feat(lib): add jwt-decode lib & googleOauth login

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,28 @@
-import '../../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { RecoilRoot } from 'recoil'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import '../../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { RecoilRoot } from 'recoil';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
+const GOOGLE_CLIENT_ID: string = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || '';
 
 export default function App({ Component, pageProps }: AppProps) {
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: Infinity
-    }
-  }
-})
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: Infinity,
+      },
+    },
+  });
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
-        {/* TODO: globalStyle 적용하기 */}
-        <Component {...pageProps} />
-      </RecoilRoot>
-    </QueryClientProvider>
-  )
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          {/* TODO: globalStyle 적용하기 */}
+          <Component {...pageProps} />
+        </RecoilRoot>
+      </QueryClientProvider>
+    </GoogleOAuthProvider>
+  );
 }
---
+++ src/pages/auth/google.tsx
@@ -0,0 +1,18 @@
+import { GoogleLogin } from '@react-oauth/google';
+
+const Google = () => {
+  return (
+    <div className="flex">
+      <GoogleLogin
+        onSuccess={(credentialResponse) => {
+          console.log(credentialResponse);
+        }}
+        onError={() => {
+          console.log('Login Failed');
+        }}
+      />
+    </div>
+  );
+};
+
+export default Google;
