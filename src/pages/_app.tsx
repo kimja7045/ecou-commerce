2892071bc626d398509ed80ac9895c384d6e781d
김재훈 <jhkim05@miridih.com>
2023-03-25 10:17:08 +0900

    fix(import): update Header import at _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import '../../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import Header from '@/components/Layout/Header';
+import Header from '@/components/AppLayout/Header';
 import { SessionProvider } from 'next-auth/react';
 import styled from '@emotion/styled';
 
