429b3dc5601088d04e0c4f488ceb2f97865c66b1
김재훈 <jhkim05@miridih.com>
2023-04-10 00:18:18 +0900

    feat(react-query): add useErrorBoundary option

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,6 +16,7 @@ export default function App({
       queries: {
         staleTime: Infinity,
         suspense: true,
+        useErrorBoundary: true,
       },
     },
   });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
+// import { useQueryErrorResetBoundary } from '@tanstack/react-query';
 import ProductListPage from './products';
 
 const Home = () => {
+  // const {reset} = useQueryErrorResetBoundary();
+
   return <ProductListPage />;
 };
 
