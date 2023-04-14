b0dba05b5782dc445228b66eedc9c73be114b1e2
김재훈 <jhkim05@miridih.com>
2023-04-15 08:23:39 +0900

    feat(product-api): add get-withList api

---
+++ src/pages/api/products/get-wishList.ts
@@ -0,0 +1,37 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
+import { getServerSession, unstable_getServerSession } from 'next-auth';
+import { authOption } from '../auth/[...nextauth]';
+
+const prisma = new PrismaClient();
+
+async function getWishList(userId: string) {
+  try {
+    const response = await prisma.wishlist.findUnique({
+      where: {
+        userId,
+      },
+    });
+    return response?.productIds.split(',');
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const session = await getServerSession(req, res, authOption);
+  if (!session) {
+    res.status(200).json([]);
+    return;
+  }
+
+  try {
+    const wishlist = await getWishList(String(session.user?.email));
+    res.status(200).json(wishlist);
+  } catch (err) {
+    res.status(400).json({ message: 'Failed' });
+  }
+}
