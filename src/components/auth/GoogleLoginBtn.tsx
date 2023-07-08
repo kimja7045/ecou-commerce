8c2dd16488fb9d6d023e7fb57556cb0854f3bef6
kimja7045 <temp7045@gmail.com>
2023-07-08 17:26:03 +0900

    chore: update googleLogin path

---
+++ src/components/auth/GoogleLoginBtn.tsx
@@ -0,0 +1,22 @@
+import { useSession, signIn, signOut } from 'next-auth/react';
+import Button from '@/components/common/Button';
+
+export default function GoogleLoginBtn() {
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <div>
+        <p>Signed in as {session.user?.email}</p>
+        <button onClick={() => signOut()}>로그아웃</button>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col">
+      <p>Not signed in</p>
+      <Button onClick={() => signIn()}>로그인</Button>
+    </div>
+  );
+}
