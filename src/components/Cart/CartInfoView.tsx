fb05672f490296645610a4497139528a499337da
kimja7045 <temp7045@gmail.com>
2023-07-08 18:26:26 +0900

    fix(Cart): update Common Button import path

--- src/components/Cart/CartInfoView.tsx
+++ src/components/Cart/CartInfoView.tsx
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
-import Button from '../common/Button';
+import Button from '@/components/common/Button';
 
 interface CartInfoViewProps {
   totalPrice: number;
